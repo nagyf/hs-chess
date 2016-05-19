210e5394da5e7a217b792727cac9a4b5901f71e1
Ferenc Nagy <jester.nf@gmail.com>
2016-05-19 16:06:31 +0200

    code documentation

--- src/SAN.hs
+++ src/SAN.hs
@@ -12,6 +12,8 @@ import Data.Map as Map
 import Board hiding (pieceType, color, pos)
 import Geometry hiding (row)
 
+-- | Represents annotations that added to moves,
+-- like '+' for check and '#' for checkmate
 data Annotation = Check
                 | CheckMate deriving (Show)
 
@@ -25,8 +27,10 @@ data Move = Move PieceColor PieceType Pos -- ^ Nd6, e8
             | EnPassant PieceColor Int Pos -- ^ exd6e.p.
             deriving (Show)
 
+-- | A chess move with the annotations attached to it (if any)
 type AnnotatedMove = (Move, [Annotation])
 
+-- | An annotated chess move with the necessary disambiguating prefix, if needed
 data DisambiguatedMove = None AnnotatedMove
                         | FromRow Int AnnotatedMove
                         | FromColumn Int AnnotatedMove
@@ -44,6 +48,7 @@ parseMoves input = case parse match "match" input of
 match :: Parser [DisambiguatedMove]
 match = concat <$> manyTill round eof
 
+-- | Parser for a round, i.e. a white and a black move
 round :: Parser [DisambiguatedMove]
 round = do
     _ <- roundNumber
@@ -52,17 +57,21 @@ round = do
     skipMany whitespace >> skipMany eol
     return [white, black]
 
+-- | A parser for round number: e.g. "3."
 roundNumber :: Parser Int
 roundNumber = do
     digits <- fmap read (many1 digit)
     _ <- char '.' >> skipMany whitespace
     return digits
 
+-- | A parser for a chess move
 move :: PieceColor -> Parser DisambiguatedMove
 move color =
-    try(disambiguatingMove color)
+    try (disambiguatingMove color)
     <|> fmap None (normalMove color)
 
+-- | A parser for a disambiguating chess move (this contains a prefix to denote
+-- the source position of the piece, in case of ambiguity)
 disambiguatingMove :: PieceColor -> Parser DisambiguatedMove
 disambiguatingMove color = do
     from <- try (oneOf columnNames) <|> oneOf ['1'..'8']
@@ -73,6 +82,7 @@ disambiguatingMove color = do
         then return (FromRow (read [from]) m)
         else return (FromColumn (columnNameToInt from) m)
 
+-- A parser for a chess move without ambiguity
 normalMove :: PieceColor -> Parser AnnotatedMove
 normalMove color = do
     m <- try(castling color)
@@ -87,36 +97,36 @@ normalMove color = do
     _ <- skipMany whitespace
     return (m, as)
 
+-- | Parser for castling moves
 castling :: PieceColor -> Parser Move
 castling color =
     try (queenSideCastle color) <|>
     try (kingSideCastle color)
 
+-- | Parser for king side castling
 kingSideCastle :: PieceColor -> Parser Move
 kingSideCastle color =
     string "0-0" >> return (KingSideCastle color)
 
+-- | Parser for Queen side castling
 queenSideCastle :: PieceColor -> Parser Move
 queenSideCastle color =
     string "0-0-0" >> return (QueenSideCastle color)
 
+-- | Parser for pawn promotion
 pawnPromotion :: PieceColor -> Parser Move
 pawnPromotion color = do
     c <- coordinate
     promoted <- pieceType
     return $ PawnPromotion color promoted c
 
-coordinate :: Parser Pos
-coordinate = do
-    col <- fmap columnNameToInt columnName
-    row <- read <$> many1 digit
-    return (col, row)
-
+-- | Parser for a normal pawn move
 pawnMove :: PieceColor -> Parser Move
 pawnMove color = do
     c <- coordinate
     return $ Move color Pawn c
 
+-- | Parser for an en passant pawn capture
 enPassant :: PieceColor -> Parser Move
 enPassant color = do
     from <- fmap columnNameToInt columnName
@@ -124,12 +134,14 @@ enPassant color = do
     _ <- string "e.p."
     return $ EnPassant color from to
 
+-- | Parser for a simple capture (except pawn and en passant)
 normalCapture :: PieceColor -> Parser Move
 normalCapture color = do
     p <- pieceType
     pos <- capturePos
     return $ Capture color p pos
 
+-- | Parser for a pawn capture
 pawnCapture :: PieceColor -> Parser Move
 pawnCapture color = do
     from <- fmap columnNameToInt columnName
@@ -142,32 +154,47 @@ standardMove color = do
     c <- coordinate
     return $ Move color p c
 
+-- | Parser for an 'x' followed by a position
 capturePos :: Parser Pos
 capturePos = char 'x' >> coordinate
 
+-- | Parser for a (Int,Int) (i.e. for a board position)
+coordinate :: Parser Pos
+coordinate = do
+    col <- fmap columnNameToInt columnName
+    row <- read <$> many1 digit
+    return (col, row)
+
+-- | Column name parser
 columnName :: Parser Char
 columnName = oneOf columnNames
 
+-- | Piece type parser
 pieceType :: Parser PieceType
 pieceType = do
     ch <- oneOf (Map.keys pieceTypes)
     return $ fromJust $ Map.lookup ch pieceTypes
 
+-- | Annotation parser
 annotations :: Parser [Annotation]
 annotations = checkMate <|> check <|> return []
 
+-- | Check annotation parser
 check :: Parser [Annotation]
 check = char '+' >> return [Check]
 
+-- | Checkmate annotation parser
 checkMate :: Parser [Annotation]
 checkMate = char '#' >> return [CheckMate]
 
+-- | End of line parser, that supports all line ending formats
 eol :: Parser String
 eol = try (string "\n\r")
     <|> try (string "\r\n")
     <|> string "\n"
     <|> string "\r"
 
+-- | Whitespace (i.e. space and tab) parser
 whitespace :: Parser Char
 whitespace = space <|> tab
 
@@ -180,6 +207,7 @@ columnNameToInt ch = fromJust $ Map.lookup ch columnNameMapping
 columnNameMapping :: Map Char Int
 columnNameMapping = Map.fromList $ zip columnNames [1..]
 
+-- | The possible column labels
 columnNames :: String
 columnNames = ['a'..'h']
 
